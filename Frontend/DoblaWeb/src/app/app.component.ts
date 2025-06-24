5b4f8da0f1f94d061a8ca929b3e1bb4049430234
Uriel-Abdallah-Medina-Torres <230768@utxicotepec.edu.mx>
2025-06-24 15:30:07 -0600

    Implementación header de login y signup
    El header esta correctamente

--- Frontend/DoblaWeb/src/app/app.component.ts
+++ Frontend/DoblaWeb/src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { BodyComponent } from './components/body/body.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { HeaderAdminComponent } from './components/header-admin/header-admin.component';
 import { Footer1Component } from './components/footer1/footer1.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, BodyComponent, FooterComponent,HeaderAdminComponent,Footer1Component],
+  imports: [HeaderComponent, BodyComponent,HeaderAdminComponent,Footer1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
