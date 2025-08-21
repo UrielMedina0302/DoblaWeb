e6154215320520772d85bf409627a14199f678d4
Uriel-Abdallah-Medina-Torres <230768@utxicotepec.edu.mx>
2025-08-21 00:48:52 -0600

    Build de Angular 19 funcionando

--- Frontend/DoblaWeb/src/app/pages/home/home.component.ts
+++ Frontend/DoblaWeb/src/app/pages/home/home.component.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
